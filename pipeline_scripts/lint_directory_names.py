6c024fbd711065a52288bf131e66977774ef39c5
Brendan OConnell <bkoconnell@gmail.com>
2023-10-08 18:02:20 -0400

    refactor arg parsing & validation

--- pipeline_scripts/lint_directory_names.py
+++ pipeline_scripts/lint_directory_names.py
@@ -9,6 +9,38 @@ import logging
 logging.basicConfig(level=logging.INFO, format=' %(asctime)s - %(levelname)s - %(message)s')
 
 
+def resolve_path(path):
+    """Resolves a given path, directory, or filename. Exception thrown if invalid path."""
+    logging.debug("resolve_path() function.")
+    try:
+        resolved_path = path.resolve(strict=True)
+    except FileNotFoundError as err:
+        msg = f"Invalid CL arg:  --root '{path}' is not a valid path."
+        logging.critical(f"{msg}\nFileNotFoundError: {err}")
+        sys.exit(2)
+    logging.info(f"--root:\t{resolved_path}")
+    return resolved_path
+
+
+def is_directory(dir):
+    """Checks that the given arg is a directory."""
+    logging.debug("is_directory() function.")
+    if not dir.is_dir():
+        msg = f"Invalid CL arg:  --root '{dir}' is not a directory."
+        logging.critical(f"{msg}")
+        sys.exit(2)
+
+
+def store_excluded_dirs(root, dirs):
+    """Store the CL args passed in for list of directories to be excluded from lint check."""
+    logging.debug("store_excluded_dirs() function.")
+    excluded = []
+    if dirs is not None:
+        excluded = [os.path.join(root, dir) for dir in dirs]
+    logging.info(f"--exclude:\t{excluded}")
+    return excluded
+
+
 def parse_CL_args():
     """Parses command-line arguments passed in."""
     logging.debug("parse_CL_args() function.")
@@ -16,42 +48,24 @@ def parse_CL_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("-r", "--root",
                         type=Path,
-                        default=('.'),
+                        default=('..'),
                         help="""
                         Optionally specify a root directory. Default='.'\n\n
                         Usage: python3 lint_directory_names.py --root "./rel/path/dir_name"\n\n
                         """)
-    parser.add_argument("-d", "--directories",
+    parser.add_argument("-e", "--exclude",
                         nargs='*',
                         help="""
                         An optional list of directory paths to exclude from lint check.\n\n
                         Usage: python3 lint_directory_names.py 
-                        --directories "rel/path/dir_1" "rel/path/dir_2\n\n"
+                        --exclude "rel/path/dir_1" "rel/path/dir_2\n\n"
                         Note: The directory path is relative to the --root directory.\n\n
                         """)
     args = parser.parse_args()
-
-    # Resolve path for 'root' arg
-    try:
-        root_arg = args.root.resolve(strict=True)
-    except FileNotFoundError as err:
-        msg = f"Invalid CL arg:  --root '{args.root}' is not a valid path."
-        logging.critical(f"{msg}\nFileNotFoundError: {err}")
-        sys.exit(2)
-    # Verify 'root' arg is a directory, not a file
-    if not root_arg.is_dir():
-        msg = f"Invalid CL arg:  --root '{args.root}' is not a directory."
-        logging.critical(f"{msg}")
-        sys.exit(2)
-    # Store 'directories' arg
-    dir_args = []
-    if args.directories is not None:
-        dir_args = [os.path.join(root_arg, d) for d in args.directories]
-
-    return root_arg, dir_args
+    return args
 
 
-def get_dirs_to_check(root_dir, excluded_dirs):
+def get_dirs_to_check(root_dir, excluded):
     """Generates a list of valid directories to be checked."""
     logging.debug("get_dirs_to_check() function.")
 
@@ -61,7 +75,7 @@ def get_dirs_to_check(root_dir, excluded_dirs):
     # Store paths for directories/sub-directories, except excluded ones
     for root, dirs, files in os.walk(root_dir):
         dirs[:] = [Path(root).joinpath(dir) 
-                   for dir in dirs if join(root, dir) not in excluded_dirs]
+                   for dir in dirs if join(root, dir) not in excluded]
         valid_directories.extend(dirs)
 
     return valid_directories
@@ -98,11 +112,14 @@ def evaluate_failures(root, failed_dirs):
 
 def main():
 
-    # Parse CL args
-    root, excluded = parse_CL_args()
+    # Parse & validate CL args
+    args = parse_CL_args()
+    root = resolve_path(args.root)
+    is_directory(root)
+    excluded_dirs = store_excluded_dirs(root, args.exclude)
 
     # Identify valid directories for lint check
-    valid_dirs = get_dirs_to_check(root, excluded)
+    valid_dirs = get_dirs_to_check(root, excluded_dirs)
 
     # Check if directories conform to Lint requirements
     lower, spaces = validate_dir_names(valid_dirs)
